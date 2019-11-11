322ccd4276d4a23b323047b448300a198897d00d
Simon Pintarelli <simon.pintarelli@cscs.ch>
2019-11-11 16:37:50 +0100

    add new version for ranger

--- var/spack/repos/builtin/packages/py-ranger/package.py
+++ var/spack/repos/builtin/packages/py-ranger/package.py
@@ -14,5 +14,6 @@ class PyRanger(PythonPackage):
     url      = "https://github.com/ranger/ranger/archive/v1.7.2.tar.gz"
 
     version('1.7.2', sha256='80917c93396e46272b6de63816d925eb708291a9f7a559d49b24c571ea0eeeb3')
+    version('1.9.2', sha256='49a2d8dc5fa7b1c0cac0fa72d4ad704fc7107dee36cb9feb325a42754774d363')
 
     depends_on('python@2.6:')
