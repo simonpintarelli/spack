384faa87162638c62062dbb09590862e6bf1b16b
Simon Pintarelli <simon.pintarelli@cscs.ch>
2021-02-05 23:52:07 +0100

    add kokkos 3.3

--- var/spack/repos/builtin/packages/kokkos/package.py
+++ var/spack/repos/builtin/packages/kokkos/package.py
@@ -17,6 +17,7 @@ class Kokkos(CMakePackage, CudaPackage):
 
     version('develop', branch='develop')
     version('master',  branch='master')
+    version('3.3.00', sha256='170b9deaa1943185e928f8fcb812cd4593a07ed7d220607467e8f0419e147295')
     version('3.2.00', sha256='05e1b4dd1ef383ca56fe577913e1ff31614764e65de6d6f2a163b2bddb60b3e9')
     version('3.1.01', sha256='ff5024ebe8570887d00246e2793667e0d796b08c77a8227fe271127d36eec9dd')
     version('3.1.00', sha256="b935c9b780e7330bcb80809992caa2b66fd387e3a1c261c955d622dae857d878")
