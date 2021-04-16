b21640f243dd16a0cbe2f3d7448353c9126dc76e
Simon Pintarelli <simon.pintarelli@cscs.ch>
2021-04-16 19:51:55 +0200

    comment scipy depends('py-setuptools@:51.0.0')
    I'm using setuptools as external package from archlinux

--- var/spack/repos/builtin/packages/py-scipy/package.py
+++ var/spack/repos/builtin/packages/py-scipy/package.py
@@ -50,7 +50,7 @@ class PyScipy(PythonPackage):
     depends_on('python@3.7:', when='@1.6:1.6.1', type=('build', 'link', 'run'))
     depends_on('python@3.7:3.9.999', when='@1.6.2:', type=('build', 'link', 'run'))
     depends_on('py-setuptools', when='@:1.6.1', type='build')
-    depends_on('py-setuptools@:51.0.0', when='@1.6.2:', type='build')
+    # depends_on('py-setuptools@:51.0.0', when='@1.6.2:', type='build')
     depends_on('py-pybind11@2.2.4:', when='@1.4.0', type=('build', 'link'))
     depends_on('py-pybind11@2.4.0:', when='@1.4.1:1.4.999', type=('build', 'link'))
     depends_on('py-pybind11@2.4.3:', when='@1.5:1.6.1', type=('build', 'link'))
