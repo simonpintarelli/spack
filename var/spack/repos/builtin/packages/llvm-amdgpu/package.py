9c974a8cab0f867043d9e3d928ba876e68db4812
Simon Pintarelli <simon.pintarelli@cscs.ch>
2022-10-23 10:11:42 +0200

    llvm-amdgpu glibc/nolinine patch fail to apply for llvm-amdgpu@5.3.0
    - not sure if they aren't needed anymore, llvm-amdgpu@5.3.0 seems to be based on
    clang 15

--- var/spack/repos/builtin/packages/llvm-amdgpu/package.py
+++ var/spack/repos/builtin/packages/llvm-amdgpu/package.py
@@ -142,7 +142,7 @@ class LlvmAmdgpu(CMakePackage):
     patch("adjust-openmp-bitcode-directory-for-llvm-link.patch", when="@5.2.0:")
 
     patch("glibc2.36.patch")
-    patch("noinline-attribute.patch")
+    patch("noinline-attribute.patch", when="@:5.2.4")
 
     conflicts("^cmake@3.19.0")
 
