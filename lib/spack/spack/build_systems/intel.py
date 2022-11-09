c977410c9b85058bd03dbe3075b8c723c8acf0e2
Simon Pintarelli <simon.pintarelli@cscs.ch>
2022-11-09 20:58:46 +0100

    patch build_sytems/intel for cray-mpich-binary

--- lib/spack/spack/build_systems/intel.py
+++ lib/spack/spack/build_systems/intel.py
@@ -848,6 +848,7 @@ class IntelPackage(PackageBase):
         elif (
             "^mpich@2:" in spec_root
             or "^cray-mpich" in spec_root
+            or "^cray-mpich-binary" in spec_root
             or "^mvapich2" in spec_root
             or "^intel-mpi" in spec_root
             or "^intel-oneapi-mpi" in spec_root
